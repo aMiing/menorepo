9d01638b774d4c9a332c62af6ec52ecaa9aa276e
wangcm1 <wangcm1@getui.com>
2022-08-15 10:27:07 +0800

    fix: bug修复

--- packages/pk1/lib/pk1.js
+++ packages/pk1/lib/pk1.js
@@ -3,7 +3,7 @@
 module.exports = pk1;
 
 function pk1() {
-    console.log('test1');
+    console.log('test');
     return "Hello from pk1";
 }
 
--- packages/pk2/lib/pk2.js
+++ packages/pk2/lib/pk2.js
@@ -3,6 +3,6 @@
 module.exports = pk2;
 
 function pk2() {
-    console.log('pkg222111')
+    console.log('pkg22')
     return "Hello from pk2";
 }
